e50d1bd63dc6ce017e490f5cc2fe108db806931b
sammyjopeters <peters.sammyjo@gmail.com>
2017-07-23 14:39:22 +1000

    paperclip seeds and config

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :byline, :author, :body)
+      params.require(:post).permit(:title, :byline, :author, :body, :main_image)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_attached_file :main_image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/post_default.png'
+  has_attached_file :main_image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: ActionController::Base.helpers.asset_path('/assets/posts/post_default.jpg')
   validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\z/
 
 end
